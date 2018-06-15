9f71c2ce08b7ca0b91c6605b752442c059752443
alisalhab19861986 <alisalhab19861986@gmail.com>
2018-06-15 15:48:29 +0200

    Fix: Create validation

--- Garage2.5/Migrations/Configuration.cs
+++ Garage2.5/Migrations/Configuration.cs
@@ -1,58 +1,44 @@
 namespace Garage2._5.Migrations
 {
-    using Garage2._5.Models;
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+	using Garage2._5.Models;
+	using System;
+	using System.Data.Entity;
+	using System.Data.Entity.Migrations;
+	using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Garage2._5.Models.Garage2_5Context>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
+	internal sealed class Configuration : DbMigrationsConfiguration<Garage2._5.Models.Garage2_5Context>
+	{
+		public Configuration()
+		{
+			AutomaticMigrationsEnabled = false;
+		}
 
-        protected override void Seed(Garage2._5.Models.Garage2_5Context context)
-        {
-            #region EXPLANATION
-            //  This method will be called after migrating to the latest version.
+		protected override void Seed(Garage2._5.Models.Garage2_5Context context)
+		{
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-            #endregion
+			var member = new[]
+			{
+				new Members { Name = "Andrew Peters" ,PhoneNumber="21434343",Address="FGSFGSGSG",Email="DFDSFSDS@FSDRFD.com"}
+			};
 
-            var member = new[]
-            {
-                new Members { Name = "Andrew Peters" ,PhoneNumber="21434343",Address="FGSFGSGSG",Email="DFDSFSDS@FSDRFD"}
-            };
+			context.Members.AddOrUpdate(
+				  p => new { p.Name, p.PhoneNumber, p.Address, p.Email }, member);
+			var types = new[] {
+				new VehicleType { TypeName = "Bus" },
+				new VehicleType { TypeName = "MotorCycle" },
+				new VehicleType { TypeName = "Car" },
+				new VehicleType { TypeName = "Boat" },
+				new VehicleType { TypeName = "Airplane" }
+			};
+			context.VehicleTypes.AddOrUpdate(s => new { s.TypeName }, types);
 
-            context.Members.AddOrUpdate(
-                  p => new {p.Name, p.PhoneNumber, p.Address, p.Email }, member);
-            var types = new[] {
-                new VehicleType { TypeName = "Bus" },
-                new VehicleType { TypeName = "MotorCycle" },
-                new VehicleType { TypeName = "Car" },
-                new VehicleType { TypeName = "Boat" },
-                new VehicleType { TypeName = "Airplane" }
-            };
-            context.VehicleTypes.AddOrUpdate(s => new { s.TypeName }, types);
+			context.SaveChanges();
 
-            context.SaveChanges();
+			context.ParkedVehicles.AddOrUpdate(
+			   e => new { e.TypeId, e.Member },
+			   new ParkedVehicles { TypeId = types[0].Id, MemberId = member[0].Id, Brand = "Volvo", Color = "Blue", RegistrationNumber = "ABC123", CheckIn = DateTime.Now }
 
-            context.ParkedVehicles.AddOrUpdate(
-               e => new { e.TypeId, e.Member },
-               new ParkedVehicles { TypeId = types[0].Id, MemberId = member[0].Id, Brand = "Volvo", Color="Blue", RegistrationNumber="ABC123",CheckIn= DateTime.Now}
-              
-           );
-        }
-    }
+		   );
+		}
+	}
 }
--- Garage2.5/Models/ParkedVehicles.cs
+++ Garage2.5/Models/ParkedVehicles.cs
@@ -10,8 +10,9 @@ namespace Garage2._5.Models
     public class ParkedVehicles
     {
         public int Id { get; set; }
-		[Required]
+		//[Required]
 		[StringLength(30)]
+		[RegValidation, Required]
 		[Display(Name = "Registration Number")]
         public string RegistrationNumber { get; set; }
 
